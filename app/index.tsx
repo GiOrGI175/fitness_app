0b6a3f34a1844340dd9fdc464e104f4cdc113882
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2026-03-24 18:54:45 +0400

    feature/started screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,62 @@
-import { Text, View } from "react-native";
+import { Image } from 'expo-image';
+import { LinearGradient } from 'expo-linear-gradient';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from 'react-native-responsive-screen';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className='flex-1 justify-end'>
+      <StatusBar style='light' />
+      <Image
+        source={require('../assets/images/welcome.jpg')}
+        style={StyleSheet.absoluteFill}
+        contentFit='cover'
+      />
+
+      <LinearGradient
+        colors={['transparent', '#18181b']}
+        style={{
+          width: wp(100),
+          height: hp(70),
+          justifyContent: 'flex-end',
+          paddingBottom: hp(6),
+          gap: hp(4),
+          alignItems: 'center',
+        }}
+        start={{ x: 0.5, y: 0 }}
+        end={{ x: 0.5, y: 0.8 }}
+      >
+        <View className='items-center'>
+          <Text
+            style={{ fontSize: hp(5) }}
+            className='text-white font-bold tracking-wide'
+          >
+            Best <Text className='text-rose-500'>Workouts</Text>
+          </Text>
+          <Text
+            style={{ fontSize: hp(5) }}
+            className='text-white font-bold tracking-wide'
+          >
+            For you
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          style={{ height: hp(7), width: wp(80) }}
+          className='bg-rose-500 items-center justify-center rounded-full border-2 border-neutral-200'
+        >
+          <Text
+            style={{ fontSize: hp(3) }}
+            className='text-white font-bold tracking-widest'
+          >
+            Get Started
+          </Text>
+        </TouchableOpacity>
+      </LinearGradient>
     </View>
   );
 }
