ae6b2ab78b18a7dd609e9b93bb7e05f34448fdfb
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2026-03-31 18:37:11 +0400

    feature carusel

--- components/ImageSlider.tsx
+++ components/ImageSlider.tsx
@@ -1,22 +1,31 @@
-import { sliderImages } from '@/constants';
 import React from 'react';
-import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
-import Carousel from 'react-native-snap-carousel';
-import ItemCard from './ItemCard';
+import { Dimensions, Text, View } from 'react-native';
+import Carousel from 'react-native-reanimated-carousel';
+const width = Dimensions.get('window').width;
 
 export default function ImageSlider() {
   return (
+    // <Carousel
+    //   data={sliderImages}
+    //   loop={true}
+    //   // autoplay={true}
+    //   renderItem={ItemCard}
+    //   // hasParallaxImages={true}
+    //   // slideWidth={wp(100)}
+    //   // firstItem={1}
+    //   // autplayInterval={4000}
+    //   itemWidth={wp(100) - 70}
+    //   slideStyle={{ display: 'flex', alignItems: 'center' }}
+    // />
     <Carousel
-      data={sliderImages}
-      loop={true}
-      autoplay={true}
-      renderItem={ItemCard}
-      hasParallaxImages={true}
-      slideWidth={wp(100)}
-      firstItem={1}
-      autplayInterval={4000}
-      itemWidth={wp(100) - 70}
-      slideStyle={{ display: 'flex', alignItems: 'center' }}
+      width={width}
+      height={200}
+      data={[1, 2, 3]}
+      renderItem={({ item }) => (
+        <View>
+          <Text>{item}</Text>
+        </View>
+      )}
     />
   );
 }
