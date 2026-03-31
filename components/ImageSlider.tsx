9e764f7eaa24819c3384e2599be8485899176278
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2026-03-31 19:04:06 +0400

    feature/carusel

--- components/ImageSlider.tsx
+++ components/ImageSlider.tsx
@@ -1,31 +1,31 @@
+import { sliderImages } from '@/constants';
 import React from 'react';
-import { Dimensions, Text, View } from 'react-native';
+import { View } from 'react-native';
 import Carousel from 'react-native-reanimated-carousel';
-const width = Dimensions.get('window').width;
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from 'react-native-responsive-screen';
+import ItemCard from './ItemCard';
 
 export default function ImageSlider() {
   return (
-    // <Carousel
-    //   data={sliderImages}
-    //   loop={true}
-    //   // autoplay={true}
-    //   renderItem={ItemCard}
-    //   // hasParallaxImages={true}
-    //   // slideWidth={wp(100)}
-    //   // firstItem={1}
-    //   // autplayInterval={4000}
-    //   itemWidth={wp(100) - 70}
-    //   slideStyle={{ display: 'flex', alignItems: 'center' }}
-    // />
-    <Carousel
-      width={width}
-      height={200}
-      data={[1, 2, 3]}
-      renderItem={({ item }) => (
-        <View>
-          <Text>{item}</Text>
-        </View>
-      )}
-    />
+    <View className='mt-4'>
+      <Carousel
+        data={sliderImages}
+        loop
+        autoPlay
+        width={wp(100)}
+        height={hp(25)}
+        mode='parallax'
+        modeConfig={{
+          parallaxScrollingScale: 0.9,
+          parallaxScrollingOffset: wp(21),
+        }}
+        renderItem={({ item, index, animationValue }) => (
+          <ItemCard item={item} animationValue={animationValue} />
+        )}
+      />
+    </View>
   );
 }
--- components/ItemCard.tsx
+++ components/ItemCard.tsx
@@ -1,10 +1,38 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import { Image } from 'expo-image';
+import type { SharedValue } from 'react-native-reanimated';
+import Animated, {
+  interpolate,
+  useAnimatedStyle,
+} from 'react-native-reanimated';
+import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
-export default function ItemCard() {
+type Props = {
+  item: any;
+  animationValue: SharedValue<number>;
+};
+
+export default function ItemCard({ item, animationValue }: Props) {
+  const animatedStyle = useAnimatedStyle(() => {
+    const scale = interpolate(
+      animationValue.value,
+      [-1, 0, 1],
+      [0.85, 1, 0.85],
+    );
+
+    return {
+      transform: [{ scale }],
+    };
+  });
+
   return (
-    <View>
-      <Text>Slide</Text>
-    </View>
+    <Animated.View
+      style={[{ height: hp(25), paddingHorizontal: 10 }, animatedStyle]}
+    >
+      <Image
+        source={item}
+        contentFit='cover'
+        style={{ width: '100%', height: '100%', borderRadius: 25 }}
+      />
+    </Animated.View>
   );
 }
